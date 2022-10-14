67c5c38b3b5228bcb602f3568cde4ca133150da8
Isaac Amaro <isaac.amaro@philips.com>
2022-10-14 16:15:15 -0300

    Added better error handling

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { filterImageFromURL, deleteLocalFiles } from './util/util';
-
-type FilteredImageQueryArguments = { image_url: string };
+import express from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
 (async () => {
   // Init the Express application
@@ -16,31 +14,35 @@ type FilteredImageQueryArguments = { image_url: string };
 
   // Endpoint to filter an image from a public url.
   app.get("/filteredimage", async (req, res) => {
-    const queryArgs: FilteredImageQueryArguments = req.query;
+    const { image_url } = req.query;
 
-    if (!queryArgs.image_url) {
+    if (!image_url) {
       return res.status(400).json({ message: "The query argument \"image_url\" is required" });
     }
 
-    const filteredpath: string = await filterImageFromURL(queryArgs.image_url);
+    try {
+      const filteredpath: string = await filterImageFromURL(image_url);
 
-    res.sendFile(filteredpath, (err) => {
-      if (err) {
-        res.status(500).json({ message: "Could not send the filtered image" });
-      }
-      deleteLocalFiles([filteredpath]);
-    });
+      res.sendFile(filteredpath, (err) => {
+        if (err) {
+          res.status(500).json({ message: "Could not send the filtered image" });
+        }
+        deleteLocalFiles([filteredpath]);
+      });
+    } catch {
+      res.status(422).json({ message: "Could not generate the filtered image" });
+    }
   });
   
   // Root Endpoint
   // Displays a simple message to the user
   app.get("/", async (req, res) => {
-    res.send("try GET /filteredimage?image_url={{}}");
+    res.send("try GET /filteredimage?image_url={url}");
   });
 
   // Start the Server
   app.listen(port, () => {
     console.log(`server running http://localhost:${port}`);
-    console.log(`press CTRL+C to stop server`);
+    console.log("press CTRL+C to stop server");
   });
 })();
